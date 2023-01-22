1fecf55fd717603ee8a8f33174505955f11f75d4
fxmbx <funbiolaore@gmail.com>
2023-01-22 02:49:08 +0100

    feat:post repository

--- models/post.entity.go
+++ models/post.entity.go
@@ -2,7 +2,7 @@ package models
 
 type Post struct {
 	ID          int32  `gorm:"primary_key:auto_increment" json:"-"`
-	Title       string `gorm:"type:text" json:"-"`
+	Title       string `gorm:"type:text;unique" json:"-"`
 	Description string `gorm:"varchar(255)" json:"-"`
 	UserID      int32  `gorm:"not null" json:"-"`
 	User        User   `gorm:"foreignkey:UserID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"-"`
---
+++ repository/post.repository.go
@@ -0,0 +1,68 @@
+package repository
+
+import (
+	"github.com/fxmbx/go_practice_pr/models"
+	"github.com/fxmbx/go_practice_pr/utils"
+	"gorm.io/gorm"
+)
+
+type PostRepository interface {
+	InserPost(post models.Post) (models.Post, error)
+	UpdatePost(post models.Post) (models.Post, error)
+	FindByTitle(title string) (models.Post, error)
+	FindByPostID(postId string) (models.Post, error)
+	ListPost(pagination utils.Pagination) (*utils.Pagination, error)
+	DeletePost(postId string) error
+}
+
+type postRepository struct {
+	db *gorm.DB
+}
+
+func NewPostRepository(dbConnectoin *gorm.DB) PostRepository {
+	return &postRepository{
+		db: dbConnectoin,
+	}
+}
+
+func (postRepo *postRepository) InserPost(post models.Post) (models.Post, error) {
+	postRepo.db.Save(&post)
+	postRepo.db.Preload("User").Find(&post)
+	return post, nil
+}
+func (postRepo *postRepository) UpdatePost(post models.Post) (models.Post, error) {
+	postRepo.db.Save(&post)
+	postRepo.db.Preload("User").Find(&post)
+	return post, nil
+}
+func (postRepo *postRepository) FindByTitle(title string) (models.Post, error) {
+	var post models.Post
+	res := postRepo.db.Preload("User").Where("title = ?", title).Take(&post)
+	if res.Error != nil {
+		return post, res.Error
+	}
+	return post, nil
+}
+func (postRepo *postRepository) FindByPostID(postId string) (models.Post, error) {
+	var post models.Post
+	res := postRepo.db.Preload("User").Where("id = ?", postId).Take(&post)
+	if res.Error != nil {
+		return post, res.Error
+	}
+	return post, nil
+}
+func (postRepo *postRepository) ListPost(pagination utils.Pagination) (*utils.Pagination, error) {
+	var posts []*models.Post
+	postRepo.db.Scopes(Paginate(posts, &pagination, postRepo.db)).Unscoped().Find(&posts)
+	pagination.Data = posts
+	return &pagination, nil
+}
+func (postRepo *postRepository) DeletePost(postId string) error {
+	var post models.Post
+	res := postRepo.db.Preload("User").Where("title = ?", postId).Take(&post)
+	if res.Error != nil {
+		return res.Error
+	}
+	postRepo.db.Delete(&post)
+	return nil
+}
